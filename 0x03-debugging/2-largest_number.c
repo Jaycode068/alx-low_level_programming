9d61598ccca1b89573d8be35a08ec26ae7a45319
Jaycode068 <ajayijohnson68@gmail.com>
2022-10-19 05:28:23 -0700

    Largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,18 +14,18 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-		largest = a;
+	largest = a;
 	}
-	else if (b > c && b > a)
+	else if (b > a && b > c)
 	{
-		largest = b;
+	largest = b;
 	}
 	else if (c > b)
 	{
-		largest = c;
+	largest = c;
 	else
 	{
-		largest = b;
+	largest = b;
 	}
 
 	return (largest);
