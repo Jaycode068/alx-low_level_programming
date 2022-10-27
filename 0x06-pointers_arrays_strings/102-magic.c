2abbf5290c4da8e0cc49ed56f2beb1fe7703ede6
Jaycode068 <ajayijohnson68@gmail.com>
2022-10-27 12:43:28 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - printing the third index
-* Return: an integer
+* main - Printing the third index
+* Return: integer
 */
-
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+	int n;
+	int a[5];
+	int *p;
 
-a[2] = 1024;
-p = &n;
+	a[2] = 1024;
+	p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -21,8 +20,8 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-*p[5] = 98;
+	p[5] = 98;
   /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
