b7dc95e2262c032e03bc1a066baa252097e056fd
Jaycode068 <ajayijohnson68@gmail.com>
2022-10-27 12:30:20 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,30 @@
 #include "main.h"
-
 /**
-* leet - encodes a string into 1337
-* @n:input
-* Return: the value of n
-*/
-char *leet(char *n)
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer dest.
+ */
+
+char *leet(char *s)
 {
+int count = 0, i;
+int lower_case[] = {97, 101, 111, 116, 108};
+int upper_case[] = {65, 69, 79, 84, 76};
+int numbers[] = {52, 51, 48, 55, 49};
 
-	int i, j;
-	char s1[] = "aAeEoOtTlL"
-	char s2[] = "4433007711"
 
-	for (i = 0; n[0] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
-		}
-	}
-	return (n);
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 5; i++)
+{
+if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
+{
+*(s + count) = numbers[i];
+break;
+}
+}
+count++;
+}
+
+return (s);
 }
