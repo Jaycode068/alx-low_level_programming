7ec96433621bbed342a10c1e7a20bdbd901c2d18
Jaycode068 <ajayijohnson68@gmail.com>
2022-11-02 08:16:34 -0700

    Comparing two strings

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int evaluate_num(int num, int iterator);
 int is_palindrome(char *s);
 int _strlen_recursion(char *s)
 int p1(char *s, int l);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
